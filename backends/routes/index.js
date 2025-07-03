e9ea32c5a40476f342e2d86eb15a6756d1dd2542
Leemxy-16 <herleemahaliyu@gmail.com>
2025-07-03 07:48:07 +0100

    Update index.js

--- backends/routes/index.js
+++ backends/routes/index.js
@@ -1,12 +1,12 @@
-const { adminRouter } = require("./adminroutes")
-const authRouter = require("./authroutes")
-const usersRouter = require("./userroutes")
+const { adminRouter } = require("./adminroutes");
+const authRouter = require("./authroutes");
+const usersRouter = require("./userroutes");
 
-const router = require("express").Router
+const { Router } = require("express");
+const baseRouter = Router();
 
-const baseRouter = router()
+baseRouter.use("/auth", authRouter);
+baseRouter.use("/users", usersRouter);
+baseRouter.use("/admin", adminRouter);
 
-baseRouter.use("/auth", authRouter)
-baseRouter.use("/users", usersRouter)
-baseRouter.use("/admin", adminRouter)
-module.exports = baseRouter
+module.exports = baseRouter;
